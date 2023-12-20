73aab2ea6706ce8ce5d9a366bb31592642b6a1b2
Philippe Matray <phmatray@gmail.com>
2023-12-20 21:43:15 +0100

    add MiniChart.razor component

---
+++ TradingViewBlazor/Components/MiniChart/MiniChart.razor.js
@@ -0,0 +1,19 @@
+export function initTradingViewComponent(reference, settings) {
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = JSON.stringify({
+        symbol: settings.symbol,
+        width: settings.width,
+        height: settings.height,
+        locale: settings.locale,
+        dateRange: settings.dateRange,
+        colorTheme: settings.colorTheme,
+        isTransparent: settings.isTransparent,
+        autosize: settings.autosize,
+        largeChartUrl: settings.largeChartUrl
+    });
+
+    reference.appendChild(script);
+}
---
+++ TradingViewBlazor/Components/MiniChart/MiniChartSettings.cs
@@ -0,0 +1,14 @@
+namespace TradingViewBlazor.Components.MiniChart;
+
+public class MiniChartSettings
+{
+    public string Symbol { get; set; } = "FX:EURUSD";
+    public string Width { get; set; } = "350";
+    public string Height { get; set; } = "220";
+    public string Locale { get; set; } = "en";
+    public string DateRange { get; set; } = "12M";
+    public string ColorTheme { get; set; } = "light";
+    public bool IsTransparent { get; set; } = false;
+    public bool Autosize { get; set; } = false;
+    public string LargeChartUrl { get; set; } = "";
+}
