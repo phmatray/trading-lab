fa36b228522f77148d4f855206a15dcade2f71d2
Philippe Matray <phmatray@gmail.com>
2026-05-06 14:05:34 +0200

    feat: add domain enums and Citation record

---
+++ TradyStrat/Shared/Domain/SuggestionAction.cs
@@ -0,0 +1,3 @@
+namespace TradyStrat.Shared.Domain;
+
+public enum SuggestionAction { Acquire = 1, Hold = 2, Trim = 3, Wait = 4 }
---
+++ TradyStrat/Shared/Domain/TradeSide.cs
@@ -0,0 +1,3 @@
+namespace TradyStrat.Shared.Domain;
+
+public enum TradeSide { Buy = 1, Sell = 2 }
