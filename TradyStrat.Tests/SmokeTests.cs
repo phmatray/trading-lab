b87eed292e5455f91fd3d66a331a3297bef67133
Philippe Matray <phmatray@gmail.com>
2026-05-06 13:43:36 +0200

    chore: scaffold solution, projects, central package versions

---
+++ TradyStrat.Tests/SmokeTests.cs
@@ -0,0 +1,10 @@
+using Xunit;
+using Shouldly;
+
+namespace TradyStrat.Tests;
+
+public class SmokeTests
+{
+    [Fact]
+    public void Compiles() => true.ShouldBeTrue();
+}
---
+++ TradyStrat/Placeholder.cs
@@ -0,0 +1,3 @@
+namespace TradyStrat;
+
+internal static class Placeholder { }
